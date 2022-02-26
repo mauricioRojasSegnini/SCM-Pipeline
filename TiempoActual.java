0a186bce7781980868ce71cc015ee2d62dd46065
mauricioRojasSegnini <56244465+mauricioRojasSegnini@users.noreply.github.com>
2022-02-25 19:26:37 -0600

    Update TiempoActual.java

--- TiempoActual.java
+++ TiempoActual.java
@@ -2,6 +2,27 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class TiempoActual {
+	
+	public static String Saludar(int hora) {
+		String saludo="";
+		if (hora < 12) {
+			saludo="Buenos días";
+		}else if (hora < 17) {
+			saludo="Buenas tarde";
+		}else {
+			saludo="Buenas noches";
+		}
+		return saludo;
+	}
+	
+	public static String DarLaHora(int hora, int min) {
+		return "Son las " + hora + ":" + min;
+	}
+	
+	public static String DarLaFecha(int dia, int mes, int anno) {
+		return "Hoy estamos " + dia + "/" + mes + "/" + anno;
+	}
+	
 	public static void main(String[] args) {
 		GregorianCalendar tiempo = new GregorianCalendar();
 		int hora = tiempo.get(Calendar.HOUR_OF_DAY);
@@ -9,15 +30,8 @@ public class TiempoActual {
 		int dia = tiempo.get(Calendar.DAY_OF_MONTH);
 		int mes = tiempo.get(Calendar.MONTH) + 1;
 		int anno = tiempo.get(Calendar.YEAR);
-	
-		if (hora < 12) {
-			System.out.println("Buenos dias");
-		}else if (hora < 17) {
-			System.out.println("Buenas tarde");
-		}else {
-			System.out.println("Buenas noches");
-		}
-		System.out.println("Son las " + hora + ":" + min);
-		System.out.println("Hoy estamos " + mes + "/" + dia + "/" + anno);
+		System.out.println(Saludar(hora));
+		System.out.println(DarLaHora(hora, min));
+		System.out.println(DarLaFecha(dia,mes,anno));
 	}
 }
